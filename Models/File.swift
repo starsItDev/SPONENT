863049c06db4ff03283cef264778b31f7bcd303a
starsItDev <144766845+starsItDev@users.noreply.github.com>
2023-09-12 14:14:45 +0500

    mm

--- Models/File.swift
+++ Models/File.swift
@@ -5,26 +5,4 @@
 //  Created by StarsDev on 12/09/2023.
 //
 
-import Foundation
-import UIKit
 
-
-struct name {
-   var a = ""
-}
-
-
-class Home: UIViewController{
-    
-}
-
-extension  Home {
-    
-  @IBInspectable  var cornerRadius: CGFloat {
-        get {
-            return self.cornerRadius
-        }
-        set {
-        }
-    }
-}
