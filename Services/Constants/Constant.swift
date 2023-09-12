27ee4d75469bdd1b738c8b4294d4a1a2da56c353
starsItDev <144766845+starsItDev@users.noreply.github.com>
2023-09-12 14:53:44 +0500

    constants

---
+++ Services/Constants/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  SPONENT
+//
+//  Created by StarsDev on 12/09/2023.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = "https://playwithmeapp.com"
+    
+    struct Endpoints {
+        static let login = "/app/login"
+        static let signup = "/app/signup"
+        static let categories = "/app/categories"
+    }
+}
+
---
+++ Services/Networking/APIManger.swift
@@ -0,0 +1,8 @@
+//
+//  APIManger.swift
+//  SPONENT
+//
+//  Created by StarsDev on 12/09/2023.
+//
+
+import Foundation
