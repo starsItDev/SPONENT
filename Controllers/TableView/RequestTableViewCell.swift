e75d25a1c8cfb8d678d0b87f3faa56536315714b
starsItDev <144766845+starsItDev@users.noreply.github.com>
2023-09-22 18:38:47 +0500

    Controller Updated

--- Controllers/TableView/RequestTableViewCell.swift
+++ Controllers/TableView/RequestTableViewCell.swift
@@ -13,7 +13,7 @@ class RequestTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        requestView.layer.borderWidth = 0.5
+        requestView.layer.borderWidth = 0.8
         requestView.layer.borderColor = UIColor.lightGray.cgColor
 //        requestView.layer.shadowColor = UIColor.black.cgColor
 //        requestView.layer.shadowOpacity = 0.2
